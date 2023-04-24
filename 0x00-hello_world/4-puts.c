df3f206b30ca0d9b00286b3cb0a3fad13b681dcd
root <root@DESKTOP-3VE4J3E.localdomain>
2023-04-24 01:32:21 +0100

    task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 
+/**
+  *main - main entry point
+  *
+  *Return: always 0
+  */
 int main(void)
 {
 	char str[] = "\"Programming is like building a multilingual puzzle";
+
 	puts(str);
 
 	return (0);
