ded41eaad65f266188238c29e90f095a60c225f5
root <root@DESKTOP-3VE4J3E.localdomain>
2023-04-24 02:40:35 +0100

    task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Return: always 1
+  */
+int main(void)
+{
+	fprintf(stderr, "nd that piece of art is useful\" - Dora Korpar, 2015-10-19");
+
+	return (1);
+}
