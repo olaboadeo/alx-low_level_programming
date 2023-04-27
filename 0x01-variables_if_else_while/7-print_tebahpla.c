c7b21c3fa832ae66c7eac0fc39bde64447d67b77
root <root@DESKTOP-3VE4J3E>
2023-04-27 07:26:14 +0100

    task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 122; a > 96; a--)
+	{
+		putchar(a);
+	}
+	putchar('\n');
+	return (0);
+}
