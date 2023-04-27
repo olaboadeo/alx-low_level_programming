b0a2dc4aee76988ddda08199ecbe7ae68b6c6bce
root <root@DESKTOP-3VE4J3E>
2023-04-27 11:21:30 +0100

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -8,6 +8,17 @@
   */
 int print_last_digit(int c)
 {
-	c = c % 10;
-	return (c);
+	int last_digit;
+
+	last_digit = c % 10;
+	if (last_digit >= 0)
+	{
+		_putchar(last_digit + 48);
+	}
+	else
+	{
+		last_digit = last_digit * -1;
+		_putchar(last_digit + 48);
+	}
+	return (last_digit);
 }
