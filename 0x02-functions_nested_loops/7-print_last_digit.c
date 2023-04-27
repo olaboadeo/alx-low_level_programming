4fb6469d7c1d53fccb8f93d9cef17d6688ef5a59
root <root@DESKTOP-3VE4J3E>
2023-04-27 05:53:25 +0100

    task 7

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  *print_last_digit - prints the last digit of a number
+  *@c: number to be printe
+  *Description: a function that prints the last digit of a number
+  *Return: the last digit of the number
+  */
+int print_last_digit(int c)
+{
+	c = c % 10;
+	return (c);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -1,3 +1,7 @@
+int print_last_digit(int);
+
+int _abs(int);
+
 int print_sign(int n);
 
 int _isalpha(int c);
