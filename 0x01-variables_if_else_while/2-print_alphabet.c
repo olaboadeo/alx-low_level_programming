3b146f95935f185e15eafd59335b811b941f6c86
root <root@DESKTOP-3VE4J3E>
2023-04-27 06:30:03 +0100

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *
+  *Return: always 0
+  */
+int main(void)
+{
+	int a = 97;
+
+	while (a <= 122)
+	{
+		putchar(a);
+		a++;
+	}
+	putchar('\n');
+	return (0);
+}
