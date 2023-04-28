24877a6ebaa8b66ac054880acde54c7ad27aea96
root <root@DESKTOP-3VE4J3E>
2023-04-28 05:43:03 +0100

    task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -6,4 +6,20 @@
   */
 void print_square(int size)
 {
+	int a, b;
 
+	if (size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+
+	for (a = 0; a < size; a++)
+	{
+		for (b = 0; b < size; b++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
