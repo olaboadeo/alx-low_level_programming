0868515700e4feb3325b7d69d30a53c654bd042c
root <root@DESKTOP-3VE4J3E>
2023-04-28 05:54:16 +0100

    task 9

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	int a;
+
+	for (a = 1; a <= 100; a++)
+	{
+		if (a % 15 == 0)
+		{
+			printf("FizzBuzz");
+		}
+		else if (a % 3 == 0)
+		{
+			printf("Fizz");
+		}
+		else if (a % 5 == 0)
+		{
+			printf("Buzz");
+		}
+		else
+		{
+			printf("%d", a);
+		}
+
+		if (a != 100)
+		{
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
