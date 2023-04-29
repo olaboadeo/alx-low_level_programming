b03849c889ec4ddea666a45193c961d5eda430d0
root <root@DESKTOP-3VE4J3E>
2023-04-29 04:59:23 +0100

    task 11

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+/**
+  *main - entry point
+  *Return: always 0
+  */
+int main(void)
+{
+	long int n = 612852475143;
+	long int factor = 2;
+	long int larg_factor = 1;
+
+	while (n > 1)
+	{
+		if (n % factor == 0)
+		{
+			larg_factor = factor;
+			n = n / factor;
+		}
+		else
+		{
+			factor++;
+		}
+	}
+
+	printf("%ld\n", larg_factor);
+	return (0);
+}	
