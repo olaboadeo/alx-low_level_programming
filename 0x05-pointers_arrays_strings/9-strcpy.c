f4bffcc523b4653c16225925f9444849ae6615d7
root <root@DESKTOP-3VE4J3E>
2023-05-04 23:41:43 +0100

    task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+  *strcpy - copies a string 
+  *@dest: destination string
+  *@src: source string
+  *Return: the pointer to dest
+  */
+char *_strcpy(char *dest, char *src)
+{
+	int l = 0;
+	int a;
+	char *p = dest;
+
+	while (src[l] != '\0')
+	{
+		l++;
+	}
+
+	for (a = 0; a <= l; a++)
+	{
+		dest[a] = src[a];
+	}
+	
+	return (p);
+}
