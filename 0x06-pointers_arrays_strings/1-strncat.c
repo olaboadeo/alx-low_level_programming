280e7e28972d17d35f7fc8d6f78d279cd0a3047d
root <root@DESKTOP-3VE4J3E>
2023-05-05 00:40:34 +0100

    task 1

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -4,8 +4,23 @@
   *_strncat - concatenates two strings
   *@dest: destination string
   *@src: source string
+  *@n: number of bytes from src
   *Return: concatenated string
   */
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int l = 0;
+	int a;
+	char *p = dest;
+
+	while (dest[l] != '\0')
+	{
+		l++;
+	}
+
+	for (a = 0; a < n; a++)
+	{
+		dest[l + a] = src[a];
+	}
+	return (p);
+}
