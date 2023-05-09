853fb45ad4c4cced429045ba9d3832bc899f3f47
root <root@DESKTOP-3VE4J3E>
2023-05-09 20:45:08 +0100

    task 7

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+  *print_diagsums - prints the sum of the two diagonals
+  *@a: square matrix
+  *@size: size of the square matrix
+  *
+  */
+void print_diagsums(int *a, int size)
+{
+	int i, j;
+	int sum1 = 0;
+	int sum2 = 0;
+
+	for (i = 0; i < size; i++)
+	{
+		for (j = 0; j < size; j++)
+		{
+			if (i == j)
+			{
+				sum1 += *(a + (i * size + j));
+			}
+
+			if ((i + j) == (size - 1))
+			{
+				sum2 += *(a + (i * size + j));
+			}
+		}
+	}
+	printf("%d, %d\n", sum1, sum2);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,6 @@
+
+void print_diagsums(int *a, int size);
+
 void print_chessboard(char (*a)[8]);
 
 char *_strstr(char *haystack, char *needle);
