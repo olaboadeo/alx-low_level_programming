536f254e24fe65781db427ac1d8b96d32671efa0
root <root@DESKTOP-3VE4J3E>
2023-05-09 02:52:31 +0100

    task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+  *_strstr - locates a substring
+  *@haystack: string to be searched
+  *@needle: string to be searched for
+  *
+  *Return: the first occurrence of the substring needle
+  *NULL if the substring is not found
+  */
+char *_strstr(char *haystack, char *needle)
+{
+	int i, j, x;
+
+	for (i = 0; haystack[i] != '\0'; i++)
+	{
+		for (j = 0; needle[j] != '\0'; j++)
+		{
+			if (haystack[i] == needle[j])
+			{
+				for (x = 0; needle[x] != '\0'; x++)
+				{
+					if (haystack[i + x] != needle[x])
+					{
+						break;
+					}
+					return (haystack + i);
+				}
+			}
+		}
+	}
+	return ('\0');
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+char *_strstr(char *haystack, char *needle);
+
 char *_strpbrk(char *s, char *accept);
 
 unsigned int _strspn(char *s, char *accept);
