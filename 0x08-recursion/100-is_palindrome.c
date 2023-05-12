640d975115661e9b845faea65b6a744dc2fafcca
root <root@DESKTOP-3VE4J3E>
2023-05-12 01:48:22 +0100

    task 7

--- 0x08-recursion/100-is_palindrome.c
+++ 0x08-recursion/100-is_palindrome.c
@@ -2,6 +2,8 @@
 
 int helper(char *a, int b, int c);
 
+int helper2(char *h, int a);
+
 /**
   *is_palindrome - checks if a string is a palindrome
   *@s: string to be checked
@@ -11,12 +13,7 @@ int helper(char *a, int b, int c);
   */
 int is_palindrome(char *s)
 {
-	int len = 0;
-
-	while (s[len] != '\0')
-	{
-		len++;
-	}
+	int len = helper2(s, 0);
 
 	if (len == 0)
 	{
@@ -27,6 +24,26 @@ int is_palindrome(char *s)
 		return (helper(s, len - 1, 0));
 	}
 }
+
+/**
+  *helper2 - calculates the length of a string
+  *@h: string given
+  *@a: length of string
+  *
+  *Return: length of string
+  */
+int helper2(char *h, int a)
+{
+	if (h[a] != '\0')
+	{
+		return (helper2(h, a + 1));
+	}
+	else
+	{
+		return (a);
+	}
+}
+
 
 /**
   *helper - helpr function
