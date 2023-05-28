8fde86cbc1a3b67179510c1c81add2fe6ccf3283
root <root@DESKTOP-3VE4J3E>
2023-05-28 18:35:56 +0100

    task 4

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,19 +11,19 @@
 int main(int argc, char *argv[])
 {
 	int i = 0;
-	int j;
+	int j, q;
 
 	for (j = 1; j < argc; j++)
 	{
-		if ((*argv[j] < 58) && (*argv[j] > 47))
+		for (q = 0; *(argv[j] + q) != '\0'; q++)
 		{
-			i += atoi(argv[j]);
+			if (!((*(argv[j] + q) < 58) && (*(argv[j] + q) > 47)))
+			{
+				printf("Error\n");
+				return (1);
+			}
 		}
-		else
-		{
-			printf("Error\n");
-			return (1);
-		}
+		i += atoi(argv[j]);
 	}
 	printf("%d\n", i);
 	return (0);
