af2b141c377cf685f99faf9eda95a745490fed06
root <root@DESKTOP-3VE4J3E>
2023-06-02 05:00:20 +0100

    task 4

--- 0x12-singly_linked_lists/list.h
+++ 0x12-singly_linked_lists/list.h
@@ -20,6 +20,8 @@ typedef struct list_s
 
 list_t *add_node(list_t **head, const char *str);
 
+void free_list(list_t *head);
+
 size_t list_len(const list_t *h);
 
 size_t print_list(const list_t *h);
