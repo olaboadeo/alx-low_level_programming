ccd26a527b9c62debc8ec0ddc33f26b0af16db79
Esther Olaboade <olaboadeo@gmail.com>
2023-08-31 01:12:19 +0100

    task 6

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+int checknum(int x, int y);
+/**
+  *is_prime_number - checks if a given number is a prime number
+  *@n: given number
+  *
+  *Return: 1 if the given number is a prime number
+  *0 if otherwise
+  */
+int is_prime_number(int n)
+{
+	if (n < 2)
+		return (0);
+
+	return (checknum(n, 2));
+}
+
+int checknum(int x, int y)
+{
+	if (x == y)
+		return (1);
+	if (x % y == 0)
+		return (0);
+	return (checknum(x, y + 1));
+}
