d912b2a4fa93470041a693eb7ac453895ce86967
Esther Olaboade <olaboadeo@gmail.com>
2023-09-06 01:31:46 +0100

    task 1

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -4,3 +4,26 @@
 /**
   *_strdup - copies a string to a newly allocated space
   *@str: pointer to string
+  *
+  *Return: pointer to a new string on success
+  *NULL on failure or if str = NULL
+  */
+char *_strdup(char *str)
+{
+	char *s;
+	int len = 0;
+	int i;
+
+	if (str == NULL)
+		return (NULL);
+
+	while (str[len])
+		len++;
+
+	s = malloc(len * sizeof(char));
+	if (s == NULL)
+		return (NULL);
+	for (i = 0; i < len; i++)
+		s[i] = str[i];
+	return (s);
+}
