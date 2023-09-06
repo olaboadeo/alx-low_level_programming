bbb507a1f743275a15b52a29576f5cd3805864b8
Esther Olaboade <olaboadeo@gmail.com>
2023-09-06 01:56:12 +0100

    task 2

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -20,22 +20,21 @@ char *str_concat(char *s1, char *s2)
 		l1++;
 	while (s2[l2])
 		l2++;
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
 
 	str = malloc((l1 * sizeof(char)) + (sizeof(char) * l2) + 1);
 	if (str == NULL)
 		return (NULL);
-	
-	if (s1 != NULL)
-	{
-		for (i = 0; i < l1; i++)
-			str[i] = s1[i];
-	}
 
-	if (s2 != NULL)
-	{
-		for (j = 0; j < l1 + l2; j++)
-			str[l1 + j] = s2[j];
-	}
+	for (i = 0; i < l1; i++)
+		str[i] = s1[i];
+
+	for (j = 0; j < l1 + l2; j++)
+		str[l1 + j] = s2[j];
 
 	str[l1 + l2] = '\0';
 	return (str);
