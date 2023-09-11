77d7a9f90607eddc489f5a96f55470c24ced1430
Esther Olaboade <olaboadeo@gmail.com>
2023-09-11 20:18:52 +0100

    task 0

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+  *struct dog - details of dog
+  *@name: name of dog
+  *@age: age of dog
+  *@owner: name of dog owner
+  */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+
+#endif
