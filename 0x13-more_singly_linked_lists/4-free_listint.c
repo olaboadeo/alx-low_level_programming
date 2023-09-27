243969d323aea5453102206342c443217f5b57c3
Esther Olaboade <olaboadeo@gmail.com>
2023-09-27 01:17:01 +0100

    task 4

---
+++ 0x13-more_singly_linked_lists/4-free_listint.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+  *free_listint - frees a list
+  *@head: pointer to node
+  */
+void free_listint(listint_t *head)
+{
+	listint_t *temp;
+
+	while (head != NULL)
+	{
+		temp = head;
+		head = temp->next;
+		free(temp);
+	}
+}
