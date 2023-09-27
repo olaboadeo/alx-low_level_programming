419542b34dbc7a1c5e917bb91c00eb54be3db17f
Esther Olaboade <olaboadeo@gmail.com>
2023-09-27 01:51:02 +0100

    task 6

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,4 +1,23 @@
 #include <stdlib.h>
-#include <string.h>
-#include <stdio.h>
 #include "lists.h"
+
+/**
+  *pop_listint - pops the head of a list
+  *@head: double pointer to head of list
+  *
+  *Return: head node’s data (n)
+  */
+int pop_listint(listint_t **head)
+{
+	listint_t *new;
+	int n;
+
+	if (*head == NULL)
+		return (0);
+
+	new = *head;
+	n = (*head)->n;
+	*head = (*head)->next;
+	free(new);
+	return (n);
+}
