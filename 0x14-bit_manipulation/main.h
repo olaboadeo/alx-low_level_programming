7f9a9c41a888d45c09afea9819806e89565afa6d
Esther Olaboade <olaboadeo@gmail.com>
2023-09-29 01:34:25 +0100

    task 3

--- 0x14-bit_manipulation/main.h
+++ 0x14-bit_manipulation/main.h
@@ -5,5 +5,6 @@ int get_bit(unsigned long int n, unsigned int index);
 unsigned int binary_to_uint(const char *b);
 void print_binary(unsigned long int n);
 int _putchar(char c);
+nt set_bit(unsigned long int *n, unsigned int index);
 
 #endif
